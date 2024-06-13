b9ef905254071ebb4edb0c3e0646312653215190
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2024-06-13 07:49:53 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lorenzotinfena.github.io',
-	base: 'digital-nomad-vademecum',
-	integrations: [
-		starlight({
-			title: 'Digital nomad vademecum',
-			social: {
-				github: 'https://github.com/lorenzotinfena/digital-nomad-vademecum',
-			},
-			sidebar: [
-				{
-					label: 'Tech',
-					items: [
-						{ label: 'tech', link: '/' },
-					],
-				}
-			],
-		}),
-	],
+  site: 'https://lorenzotinfena.github.io',
+  base: 'digital-nomad-vademecum',
+  integrations: [
+    starlight({
+    components: {
+      Footer: './src/components/footer.astro'
+    },
+    title: 'Digital nomad vademecum',
+    social: {
+      github: 'https://github.com/lorenzotinfena/digital-nomad-vademecum'
+    },
+    sidebar: [{
+      label: 'Getting started',
+      link: '/'
+    }, {
+      label: 'Possessions',
+      items: [{
+        label: 'tech',
+        link: '/possessions/tech'
+      }]
+    }]
+  })]
 });
