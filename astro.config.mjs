bb92dd4ec95638f2cd82927516115fb81d3e88e0
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2024-06-23 23:03:52 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://digital-nomad-vademecum.com',
   integrations: [
